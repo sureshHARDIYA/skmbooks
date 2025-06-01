d90230a40d9d48e8c3a837183c9afa5dabce02c5
sureshHARDIYA <itsmeskm99@gmail.com>
2025-06-01 22:18:16 +0200

    Add query by slug

--- books/views.py
+++ books/views.py
@@ -1,7 +1,20 @@
+import uuid
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
 from .models import Book
 from .serializers import BookSerializer
 
 class BookListView(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('created_at')
     serializer_class = BookSerializer
+
+    def get_object(self):
+        lookup_value = self.kwargs.get('pk')  # or 'slug' depending on URL conf
+        try:
+            uuid_val = uuid.UUID(lookup_value, version=4)
+            condition = Q(id=uuid_val)
+        except ValueError:
+            condition = Q(slug=lookup_value)
+        return get_object_or_404(self.get_queryset(), condition)
--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,20 @@
+import uuid
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+
 from .models import Quiz
 from .serializers import QuizSerializer
 
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
+
+    def get_object(self):
+        lookup_value = self.kwargs.get('pk')  # or 'slug' depending on URL conf
+        try:
+            uuid_val = uuid.UUID(lookup_value, version=4)
+            condition = Q(id=uuid_val)
+        except ValueError:
+            condition = Q(slug=lookup_value)
+        return get_object_or_404(self.get_queryset(), condition)
