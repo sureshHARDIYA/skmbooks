a1507ceda8aec9d41ddb4175bb33de765f97af36
sureshHARDIYA <itsmeskm99@gmail.com>
2025-06-04 21:49:03 +0200

    Update settings.py

--- skmbooks/settings.py
+++ skmbooks/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zj_&$+h-v8l=feijtias^^%nzvqp6(&cjgr&@8jv!j+(k8mjad'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['skmbooks.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
